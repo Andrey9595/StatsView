1d76ce4082f0cf3d08875f48a0ca0c52d662d576
Andrey9595 <anb95.95@mail.ru>
2023-03-30 22:24:04 +0300

    Dot

--- app/src/main/java/ru/anb/statsview/MainActivity.kt
+++ app/src/main/java/ru/anb/statsview/MainActivity.kt
@@ -4,15 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.anb.statsview.ui.StatsView
 
-class MainActivity :  AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         findViewById<StatsView>(R.id.stats).data = listOf(
-            0.25F,
-            0.25F,
-            0.25F,
-            0.25F,
+            500F,
+            500F,
+            500F,
+            500F,
         )
     }
 }
